69f94dc363be21b39df36e6bec660a241404622e
RichardRodi <RichardmRodi@gmail.com>
2024-02-16 12:04:15 -0500

    Added Favorites

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -4,7 +4,21 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            
+            {
+                Console.WriteLine("Hello - What is your first name?");
+                var userName = Console.ReadLine();
+                
+                Console.WriteLine($"Hi, {userName} what is your favorite color?");
+                var color = Console.ReadLine();
+
+                Console.WriteLine($"{color} is a sweet color! What is your favorite Animal?");
+                var animal = Console.ReadLine();
+
+                Console.WriteLine($"{animal} is a great animal, What is your favorite band?");
+                var band = Console.ReadLine();
+
+            }
         }
     }
 }
