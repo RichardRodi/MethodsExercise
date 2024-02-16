b8136703cc1027e82a5bb4ce2012a59c361a962f
RichardRodi <RichardmRodi@gmail.com>
2024-02-16 13:15:23 -0500

    Finished exercise 1 and 2

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -1,11 +1,39 @@
-﻿namespace MethodsExercise
+﻿using System.ComponentModel;
+
+namespace MethodsExercise
 {
     public class Program
     {
+        public static int Sum(int num1, int num2)
+        {
+            var answer = num1 + num2;
+            return answer;
+        }
+        public static int Multiply(int num1, int num2, int num3)
+        {
+            return num1 * num2 * num3;
+
+        }
+        public static int Minus(int num1, int num2)
+        {  
+            return num1 - num2; 
+        
+        } 
+
+        public static int Divide(int num1, int num2)
+
+        { 
+            return num1 / num2; 
+        }
+  
+      
+
         static void Main(string[] args)
         {
             
             {
+                //Exercise 1
+
                 Console.WriteLine("Hello - What is your first name?");
                 var userName = Console.ReadLine();
                 
@@ -17,6 +45,31 @@
 
                 Console.WriteLine($"{animal} is a great animal, What is your favorite band?");
                 var band = Console.ReadLine();
+
+                Console.WriteLine($"Thanks, {userName}! Here is your Profile!");
+
+                Console.WriteLine($"Name:{userName}");
+                Console.WriteLine($"Favorite Band: {band}");
+                Console.WriteLine($"Favorite Animal: {animal}");
+                Console.WriteLine($"Favorite Color: {color}");
+
+                //Exercise 2
+
+                var amountOfCars = Sum(2, 6);
+
+                var amountOfDrivers = Multiply(60, 2, 4);
+
+                var costOfCarInsurance = Minus(100, 50);
+
+                var avgCost = Divide(100, 50);
+
+
+                Console.WriteLine();
+
+                Console.WriteLine($"The Amount of cars and drivers on the road is: {amountOfCars + amountOfDrivers} The cost of car insurance is: {costOfCarInsurance}. The average cost is: {avgCost} ").;
+
+                
+                
 
             }
         }
--- MethodsExercise/obj/Debug/net6.0/MethodsExercise.AssemblyInfo.cs
+++ MethodsExercise/obj/Debug/net6.0/MethodsExercise.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MethodsExercise")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+472d02f9d75aac5a20b813b93fd9e5787f6c5186")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+69f94dc363be21b39df36e6bec660a241404622e")]
 [assembly: System.Reflection.AssemblyProductAttribute("MethodsExercise")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MethodsExercise")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
